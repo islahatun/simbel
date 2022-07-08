f6d53ca1198cf833f7f98b45ddce9f6ae60808ac
islahatun <islahatunnufusi98@gmail.com>
2022-07-08 11:37:07 +0700

    perbaikan update

--- application/controllers/MasterBarang.php
+++ application/controllers/MasterBarang.php
@@ -154,6 +154,8 @@ class masterBarang extends CI_Controller
             </div>');
                 redirect('MasterBarang');
             }
+        } else {
+            echo "error";
         }
     }
 
--- application/views/masterBarang/formupdate.php
+++ application/views/masterBarang/formupdate.php
@@ -42,7 +42,7 @@
                                 <input type="hidden" class="form-control form-control-sm" required id="id_po" value="<?= $MasterBarang->id_po ?>">
                                 <label for="" class="col-sm-2 col-form-label-sm">Nama Barang</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control form-control-sm" required id="nama_barang" placeholder="Nama Barang" value="<?= $MasterBarang->nama_barang ?>">
+                                    <input type="text" class="form-control form-control-sm" required id="nama_barang" name="nama_barang" placeholder="Nama Barang" value="<?= $MasterBarang->nama_barang ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
@@ -59,46 +59,46 @@
                             <div class="form-group row">
                                 <label for="" class="col-sm-2 col-form-label-sm">Satuan</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control form-control-sm" required id="satuan" placeholder="Satuan" value="<?= $MasterBarang->satuan ?>">
+                                    <input type="text" class="form-control form-control-sm" required id="satuan" name="satuan" placeholder="Satuan" value="<?= $MasterBarang->satuan ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="" class="col-sm-2 col-form-label-sm">Stok</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control form-control-sm" required id="stok" placeholder="Stok" value="<?= $MasterBarang->stok ?>">
+                                    <input type="text" class="form-control form-control-sm" required id="stok" name="stok" placeholder="Stok" value="<?= $MasterBarang->stok ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="" class="col-sm-2 col-form-label-sm">Deskripsi</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control form-control-sm" required id="deskripsi" placeholder="Deskripsi" value="<?= $MasterBarang->deskripsi ?>">
+                                    <input type="text" class="form-control form-control-sm" required id="deskripsi" name="deskripsi" placeholder="Deskripsi" value="<?= $MasterBarang->deskripsi ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="" class="col-sm-2 col-form-label-sm">Harga Beli</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control form-control-sm" required id="harga_beli" placeholder="Harga Beli" value="<?= $MasterBarang->harga_beli ?>">
+                                    <input type="text" class="form-control form-control-sm" required id="harga_beli" placeholder="Harga Beli" name="harga_beli" value="<?= $MasterBarang->harga_beli ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="" class="col-sm-2 col-form-label-sm">Harga Jual</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control form-control-sm" required id="harga_jual" placeholder="Harga Jual" value="<?= $MasterBarang->harga_jual ?>">
+                                    <input type="text" class="form-control form-control-sm" required id="harga_jual" name="harga_jual" placeholder="Harga Jual" value="<?= $MasterBarang->harga_jual ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <label for="" class="col-sm-2 col-form-label-sm">Tanggal Datang</label>
                                 <div class="col-sm-10">
-                                    <input type="date" class="form-control form-control-sm" required id="tanggal_datang" placeholder="Tanggal Datang" value="<?= $MasterBarang->tanggal_datang ?>">
+                                    <input type="date" class="form-control form-control-sm" required id="tanggal_datang" name="tanggal_datang" placeholder="Tanggal Datang" value="<?= $MasterBarang->tanggal_datang ?>">
                                 </div>
                             </div>
                             <div class="form-group row">
                                 <div class="col">
-                                    <img src="<?= base_url('assets/img/profil/') ?>" alt="" class="img-thumbnail">
+                                    <img src="<?= base_url('assets/img/barang/') ?>" alt="" class="img-thumbnail">
                                 </div>
                                 <div class="col">
                                     <div class="custom-file ">
-                                        <input type="file" class="custom-file-input custom-file-input-sm" name="foto" id="foto" name="foto">
+                                        <input type="file" class="custom-file-input custom-file-input-sm" name="foto" id="foto">
                                         <label class="custom-file-label custom-file-label-sm" for="foto">Pilih Foto</label>
                                     </div>
                                 </div>
