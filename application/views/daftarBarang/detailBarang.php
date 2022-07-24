e87aabeb075be0b0e13c7d73bb252fccf23f5497
islahatun <islahatunnufusi98@gmail.com>
2022-07-24 14:52:28 +0700

    perbaikan data

--- application/views/daftarBarang/detailBarang.php
+++ application/views/daftarBarang/detailBarang.php
@@ -106,23 +106,23 @@
                                             <div class="form-group row">
                                                 <label for="" class="col-sm-4 col-form-label-sm">Jumlah Beli</label>
                                                 <div class="col-sm-8">
-                                                    <input type="text" class="form-control form-control-sm" id="count" placeholder="Jumlah Beli" value="1" name="jumlah_beli">
+                                                    <input type="text" class="form-control form-control-sm count" id="count" placeholder="Jumlah Beli" value="1" name="jumlah_beli">
                                                 </div>
-                                                <input type="hidden" value="<?= $detail['id_po'] ?>" id="id_po" name="id_po">
+                                                <input type="hidden" value="<?= $detail['id_po'] ?>" id="id_po" name="id_po" class="id_po">
                                                 <input type="hidden" value="<?= $detail['harga_jual'] ?>" id="harga_jual" name="harga_barang">
                                             </div>
                                             <div>
                                                 <div class="form-group row mt-3 mb-3">
                                                     <label for="" class="col-sm-4 col-form-label-sm">Ongkir (JNT)</label>
                                                     <div class="col-sm-8">
-                                                        <input type="text" class="form-control-plaintext form-control-plaintext-sm" id="ongkir" name="ongkir">
-                                                        <input type="hidden" class="form-control-plaintext form-control-plaintext-sm" value="<?= $detail['berat_barang'] ?>" id="berat_barang" name="berat_barang">
+                                                        <input type="text" class="form-control-plaintext form-control-plaintext-sm ongkir" id="ongkir" name="ongkir">
+                                                        <input type="hidden" class="form-control-plaintext form-control-plaintext-sm berat_barang" value="<?= $detail['berat_barang'] ?>" id="berat_barang" name="berat_barang">
                                                     </div>
                                                 </div>
                                                 <div class="form-group row mt-3 mb-3">
                                                     <label for="" class="col-sm-4 col-form-label-sm">Sub total</label>
                                                     <div class="col-sm-8">
-                                                        <input type="text" class="form-control-plaintext form-control-plaintext-sm" id="subtotal" value="<?= $detail['harga_jual'] ?>" name="total_pembelian">
+                                                        <input type="text" class="form-control-plaintext form-control-plaintext-sm subtotal" id="subtotal" value="<?= $detail['harga_jual'] ?>" name="total_pembelian">
                                                     </div>
                                                 </div>
                                             </div>
@@ -137,6 +137,11 @@
                                 <td>
                                     <form action="<?= base_url('DaftarBarang/keranjang') ?>" method="post">
                                         <input type="hidden" value="<?= $detail['id_po'] ?>" id="id_po" name="id_po">
+                                        <input type="hidden" value="<?= $detail['harga_jual'] ?>" id="harga_barang" name="harga_barang">
+                                        <input type="hidden" class="form-control form-control-sm" id="count" placeholder="Jumlah Beli" value="1" name="jumlah_beli">
+                                        <input type="hidden" class="form-control-plaintext form-control-plaintext-sm ongkir" id="ongkir" name="ongkir">
+                                        <input type="hidden" class="form-control-plaintext form-control-plaintext-sm berat_barang" value="<?= $detail['berat_barang'] ?>" id="berat_barang" name="berat_barang">
+                                        <input type="hidden" class="form-control-plaintext form-control-plaintext-sm subtotal" id="subtotal" value="<?= $detail['harga_jual'] ?>" name="total_pembelian">
                                         <button class="btn btn-outline-secondary" type="submit">keranjang</button>
                                     </form>
                                 </td>
@@ -164,20 +169,20 @@
 
     <script>
         $(document).ready(function() {
-            var berat = parseInt($("#berat_barang").val()) * 9500;
-            var ongkir = $("#ongkir").val(berat);
-            var sub_total = (parseInt($("#harga_jual").val()) + parseInt(berat));
-            $("#subtotal").val(format_number(sub_total));
+            var berat = parseInt($(".berat_barang").val()) * 9500;
+            var ongkir = $(".ongkir").val(berat);
+            var sub_total = (parseInt($(".harga_jual").val()) + parseInt(berat));
+            $(".subtotal").val(format_number(sub_total));
         });
 
         function Klik() {
             $('#ulasan').show()
         }
 
-        $('#count').on('keyup', function() {
+        $('.count').on('keyup', function() {
             var input = parseInt($(this).val());
-            subtotal = (input * $("#harga_jual").val()) + parseInt(($("#berat_barang").val() * 9500));
-            $("#subtotal").val(subtotal);
+            subtotal = (input * $(".harga_jual").val()) + parseInt(($(".berat_barang").val() * 9500));
+            $(".subtotal").val(subtotal);
         });
 
         function keranjang() {
